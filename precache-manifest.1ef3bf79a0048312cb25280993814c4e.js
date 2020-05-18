f9460c965e00be3b5db6bec22c72dfb86da89ed6
yinglung174 <ngyinglung@Ngs-MacBook.local>
2020-05-18 20:20:27 +0800

    Updates

---
+++ precache-manifest.1ef3bf79a0048312cb25280993814c4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59691d2b71796565b67fac4f855e77e5",
+    "url": "/personalized-music-box/index.html"
+  },
+  {
+    "revision": "60d6078824bd454e67c8",
+    "url": "/personalized-music-box/static/css/2.6943d46a.chunk.css"
+  },
+  {
+    "revision": "60d6078824bd454e67c8",
+    "url": "/personalized-music-box/static/js/2.d1dd59df.chunk.js"
+  },
+  {
+    "revision": "566f059e01da9e04cf73",
+    "url": "/personalized-music-box/static/js/main.483c94e4.chunk.js"
+  },
+  {
+    "revision": "a5c00b84e5ac33f4e1a3",
+    "url": "/personalized-music-box/static/js/runtime-main.d496266d.js"
+  }
+]);
